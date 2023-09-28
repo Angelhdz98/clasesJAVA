3c73a26234c23f63254fb7255f43d5e57a88a6ab
Angelhdz98 <hernandeztorresjoseangel@gmail.com>
2023-09-28 14:02:23 -0600

    Aquí realizamos nuestro primer hola mundo generamos el prime package y la primer clase y nuestro primer metodo main

---
+++ holaMundo/HolaMundo.java
@@ -0,0 +1,16 @@
+package holaMundo;
+
+public class HolaMundo {
+
+	public static void main (String[] args) {//args parece ser el nombre del array de strings, y como que es la variable de entrada que recibe el metodo
+System.out.println("Hola mundo .____.");
+
+		
+	}
+}
+
+
+
+/*
+  comentario multilinea
+  */
