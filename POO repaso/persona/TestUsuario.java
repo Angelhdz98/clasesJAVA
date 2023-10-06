2a89626f250c761d321314f13cdc0f1f73fb66b6
Angelhdz98 <hernandeztorresjoseangel@gmail.com>
2023-10-05 21:27:32 -0600

    realice la tarea de prueba de setters con los 3 casos solicitados

--- POO repaso/persona/TestUsuario.java
+++ POO repaso/persona/TestUsuario.java
@@ -6,7 +6,41 @@ public class TestUsuario {
 		// TODO Auto-generated method stub
 // instanciamos la clase usuario
 		Usuario pepe= new Usuario("paciente", "pepelon", "bicicleta123");
-
+		
+		/*
+		 * prueba de el getter para obtener la contraseña con el nombre de usuario
+		 * */
+		
+		// testeo de la función recibiendo el valor correcto
+		System.out.println(pepe.getPassword("pepelon"));
+		
+		//testeo de la función recibiendo un valor incorrecto
+		System.out.println("******************\n");
+		System.out.println(pepe.getPassword("pepelin"));
+		
+		/*
+		 * prueba del setter para cambiar la contraseña 
+		 * 
+		 * */
+		//testeo correcto de la función setter
+		System.out.println("*******************\n");
+		pepe.setPassword("contraseña");
+		//compruebo el cambio de contraseña
+		System.out.println(pepe.getPassword("pepelon"));
+		
+		//pruebas erroneas
+		
+		
+		//inicio con prueba utilizando una cadena vacía 
+		pepe.setPassword("");
+		System.out.println(pepe.getPassword("pepelon"));
+		
+		
+		//inicio con prueba utilizando una cadena vacía 
+				pepe.setPassword("contraseña");
+				System.out.println(pepe.getPassword("pepelon"));
+		
+	
 	}
 
 }
